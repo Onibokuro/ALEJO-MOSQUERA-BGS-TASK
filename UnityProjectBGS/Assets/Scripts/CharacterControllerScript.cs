f3963c191d7e97df679044b4c2c971496e320020
Onibokuro <132405405+Onibokuro@users.noreply.github.com>
2024-05-20 14:11:14 +0800

    character and clothes sprite and animation resolver

--- UnityProjectBGS/Assets/Scripts/CharacterControllerScript.cs
+++ UnityProjectBGS/Assets/Scripts/CharacterControllerScript.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class CharacterController : MonoBehaviour
+public class CharacterControllerScript : MonoBehaviour
 {
     public Vector2 MovementSpeed = new Vector2(100.0f, 100.0f); 
     private new Rigidbody2D rigidbody2D; 
     private Vector2 inputVector = new Vector2(0.0f, 0.0f);
+    [SerializeField] private bool isWalking;
     [SerializeField] private Animator[] animators;
 
     void Awake()
@@ -33,12 +34,22 @@ public class CharacterController : MonoBehaviour
     {
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
-            foreach(Animator animator in animators)
+            isWalking = true;
+            foreach (Animator animator in animators)
             {
                 animator.SetFloat("x", Input.GetAxis("Horizontal"));
                 animator.SetFloat("y", Input.GetAxis("Vertical"));
+                animator.SetBool("isWalking", true);
             }
             
+        }
+        else
+        {
+            isWalking = false;
+            foreach (Animator animator in animators)
+            {
+                animator.SetBool("isWalking", false);
+            }
         }
     }
 }
